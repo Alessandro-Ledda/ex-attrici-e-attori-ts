79eace6ec483467c9b161605f75cffc9b4efaea6
Alessandro Ledda <aleledda75@icloud.com>
2026-05-24 10:38:08 +0200

    feat type Person, type Actress + controllo proprieta dei dati + chiamata api con gestione errori in file main.ts

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+// creazione alias
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biografy: string,
+  image: string
+}
+
+
+type ActressNetionality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "israeli-american"
+  | "south-African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South-Korea"
+  | "Chinese"
+
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: ActressNetionality
+
+}
+// ritorna un booleano
+function isActress(dati: unknown): dati is Actress {
+  return (
+    // condizione di esistenza dati
+    typeof dati === 'object' && dati !== null &&
+    // controllo proprieta id
+    "id" in dati && typeof dati.id === 'number' &&
+    // controllo proprieta name
+    "name" in dati && typeof dati.name === 'string' &&
+    // controllo anno di nascita
+    "birth_year" in dati && typeof dati.birth_year === 'number' &&
+    // controllo anno di morte
+    "death_year" in dati && typeof dati.death_year === 'number' &&
+    // controllo biografia
+    "biografy" in dati && typeof dati.biografy === 'string' &&
+    // controllo image
+    "image" in dati && typeof dati.image === 'string' &&
+    "most_famous_movies" in dati && dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in dati && typeof dati.awards === 'string' &&
+    "nationality" in dati && typeof dati.nationality === 'string'
+
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const res = await fetch(`http://localhost:3333/actresses/${id}`);
+    const dati: unknown = await res.json();
+    if (!isActress(dati)) {
+      throw new Error('il formato dei dati non è valido')
+    }
+    return dati;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('errore durante il recupero attrice :', error)
+    } else {
+      console.error('Errore non definito :', error)
+    }
+    return null;
+  }
+
+}
